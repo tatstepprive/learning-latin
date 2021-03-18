356241568f8732e9b931cf47c9f764a5e58f88da
tatstepprive <tatstep@hotmail.com>
2021-03-18 22:14:44 +0100

    tatiana: using postgres db

--- src/main/resources/postgres-sql/06-create-knowledge.sql
+++ src/main/resources/postgres-sql/06-create-knowledge.sql
@@ -9,7 +9,7 @@ CREATE TABLE knowledge (
   test_id int DEFAULT NULL,
   word_id int DEFAULT NULL,
   student_input_id int DEFAULT NULL,
-  ok int DEFAULT NULL,
+  ok boolean DEFAULT NULL,
 
   FOREIGN KEY (test_id)
   REFERENCES test (id)
